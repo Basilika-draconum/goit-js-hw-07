d73824e9922a18c8d3eb1abcd16458dc3893eb2f
Lika <14dubinenkoanzhelika@gmail.com>
2022-12-27 22:59:47 +0200

    add task 7-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+galleryList.addEventListener("click", onClickImage);
+
+const createMarkUp = createImageMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", createMarkUp);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+function createImageMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+function onClickImage(e) {
+  e.preventDefault();
+}
